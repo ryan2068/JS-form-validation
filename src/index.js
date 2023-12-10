f20747ad568f815c68042ba0a388a703cb533524
ryan2068 <ryannkan@gmail.com>
2023-12-10 15:15:56 -0500

    done

--- src/index.js
+++ src/index.js
@@ -4,16 +4,59 @@ const zipCodeInput = document.querySelector('#zip-code')
 const passwordInput = document.querySelector('#password')
 const passwordConfirmationInput = document.querySelector('#password-confirmation')
 // spans for errors
+const emailSpan = document.querySelector('.email-span')
+const countrySpan = document.querySelector('.country-span')
+const zipCodeSpan = document.querySelector('.zip-code-span')
+const passwordSpan = document.querySelector('.password-span')
+const passwordConfirmationSpan = document.querySelector('.password-confirmation-span')
 
+// function to add error class to entered span
+const emptyString = (span) => {
+    span.textContent = 'Please enter in the correct format'
+    span.className = "error"
+}
 
-
-emailInput.addEventListener('input', (e) => {
+emailInput.addEventListener('input', () => {
     if (emailInput.validity.valid ) {
-        errorSpan1.textContent = ""
+        emailSpan.textContent = ""
     }
     else {
-        emptyString(errorSpan1)
+        emptyString(emailSpan)
     }
-
 })
 
+countryInput.addEventListener('input', () => {
+    if (countryInput.validity.valid ) {
+        countrySpan.textContent = ""
+    }
+    else {
+        emptyString(countrySpan)
+    }
+})
+
+zipCodeInput.addEventListener('input', () => {
+    if (zipCodeInput.validity.valid ) {
+        zipCodeSpan.textContent = ""
+    }
+    else {
+        emptyString(zipCodeSpan)
+    }
+})
+
+passwordInput.addEventListener('input', () => {
+    if (passwordInput.validity.valid ) {
+        passwordSpan.textContent = ""
+    }
+    else {
+        emptyString(passwordSpan)
+    }
+})
+
+passwordConfirmationInput.addEventListener('input', () => {
+    if (passwordConfirmationInput.validity.valid ) {
+        passwordConfirmationSpan.textContent = ""
+    }
+    else {
+        emptyString(passwordConfirmationSpan)
+    }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
- devtool: 'inline-source-map',
-  plugins: [
-  ],
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   output: {
-    filename: 'main.bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      }
+    ]
+  }
 };
